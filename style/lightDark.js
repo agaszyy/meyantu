c6855563d24ea9ff064b5cfa0f7e4dcf3ac75374
agaszyy <theagsons@gmail.com>
2025-02-17 21:12:18 +0100

    Add files via upload

---
+++ style/lightDark.js
@@ -0,0 +1,9 @@
+if (!document.documentElement.hasAttribute('data-theme')) {
+	document.documentElement.setAttribute('data-theme', 'light')
+}
+
+// const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+// document.documentElement.setAttribute(
+// 	'data-theme',
+// 	prefersDark ? 'dark' : 'light'
+// )
